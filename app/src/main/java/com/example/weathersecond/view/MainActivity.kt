6bbc8badb366315c67a2d82820507f4f4781fffa
SergeYudkin <87810884+SergeYudkin@users.noreply.github.com>
2023-01-08 19:22:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/weathersecond/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.weathersecond.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weathersecond.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
