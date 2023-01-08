f7713c3f03cb2b4501aa2e2b541fe2ce4b1f1ff3
SergeYudkin <87810884+SergeYudkin@users.noreply.github.com>
2023-01-08 20:24:58 +0300

    Добавили viewBinding

--- app/src/main/java/com/example/weathersecond/view/MainActivity.kt
+++ app/src/main/java/com/example/weathersecond/view/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.weathersecond.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.weathersecond.R
+import com.example.weathersecond.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
